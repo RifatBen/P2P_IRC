76d0e6f4354cb134c9614fd04cc19342be61a027
rifat <rifat@localhost.localdomain>
2019-03-31 16:24:58 +0100

    Ajout des structures

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "structs.h"
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+
+int main(){
+
+
+}
---
+++ structs.c
@@ -0,0 +1,21 @@
+#include "structs.h"
+
+
+TLV *newTLV(int type, int length, char *body){
+	TLV *message = calloc(1,sizeof(TLV));
+	message->type = type;
+	message->length = length;
+	strncpy(message->body,body,4078);
+
+	return message;
+}
+
+Voisins *newVoisin(int id, int port){
+	Voisins *voisin = calloc(1,sizeof(Voisins));
+	snprintf(voisin->id,8,"%d",id);
+	snprintf(voisin->port,5,"%d",port);
+	
+	voisin->next=NULL;
+
+	return voisin;
+}

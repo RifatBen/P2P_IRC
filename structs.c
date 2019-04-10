cb03207e5b8555ab23926ac6f342e210dbb7bb78
MEZIANE wacim <mezianew@informatique.univ-paris-diderot.fr>
2019-04-10 16:44:09 +0200

    jnho

--- structs.c
+++ structs.c
@@ -3,7 +3,7 @@
 
 
 Voisins *newVoisin(uint8_t id, uint8_t port){
-	Voisins *v=malloc(sizeof(Voisin));
+	Voisins *v=malloc(sizeof(Voisins));
 	v->id=id;
 	v->port=port;
 	v->symetrique=0;
@@ -11,4 +11,15 @@ Voisins *newVoisin(uint8_t id, uint8_t port){
 return v;
 }
 
-void addVoisns(Voisins *list,new Voisins){
+void addVoisin(Voisins *list, Voisins *newVoisin){
+	list->last->next = newVoisin;
+	list->last = newVoisin;
+}
+	
+Peer *initPeer(Peer p){
+	p->id=NULL;
+	p->*potentiel=NULL//ca prend une liste de voisin
+	p->*recent=NULL
+return p;
+}
+
